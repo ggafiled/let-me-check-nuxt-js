cb4b06049cdea0b062e46c5ff9df3f6559cd8d33
ggafiled <gafewkik234@gmail.com>
2021-06-03 15:18:52 +0700

    upgrade webpack lib version

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from "vuetify/es5/util/colors";
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -84,17 +82,7 @@ export default {
     customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      themes: {}
     }
   },
 
