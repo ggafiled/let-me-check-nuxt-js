c89e3e3654433406cd01428a1752d25e3a37f7b6
ggafiled <gafewkik234@gmail.com>
2021-06-03 17:01:55 +0700

    fix liff login redirect bug

--- middleware/IsAuthenticated.js
+++ middleware/IsAuthenticated.js
@@ -1,4 +1,4 @@
-export default function({ store, redirect }) {
+export default function({ store, redirect, app }) {
   // If the user is not authenticated
   if (!app.$cookies.get("auth") && !app.$cookies.get("userId")) {
     return redirect("/register");
