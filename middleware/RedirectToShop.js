0cd386f069e989a3dfd8789b528d3ad3dd287cf0
ggafiled <gafewkik234@gmail.com>
2021-06-04 07:19:18 +0700

    delete else cuase in RedirectToShop middleware.

--- middleware/RedirectToShop.js
+++ middleware/RedirectToShop.js
@@ -1,6 +1,9 @@
 export default function({ store, redirect, app }) {
   // If the user is not authenticated
-  if (window.localStorage.getItem("authenticated")) {
+  if (
+    store.state.authenticated.userId.length &&
+    store.state.authenticated.auth
+  ) {
     return redirect("/myshop");
   }
 }
