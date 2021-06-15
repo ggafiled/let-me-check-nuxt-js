7dba21fee4ae2ab6f83385ee50b29a7804f0c66e
ggafiled <gafewkik234@gmail.com>
2021-06-15 08:19:44 +0700

    fix moment timezone.

--- server-middleware/push-message.js
+++ server-middleware/push-message.js
@@ -7,35 +7,42 @@ require("dotenv").config();
 moment.locale("th");
 
 app.use(
-    express.urlencoded({
-        extended: false
-    })
+  express.urlencoded({
+    extended: false
+  })
 );
 app.use(bodyParser.json());
 
 function generateMessageStructure(to, message) {
-    let template = {
-        to: to,
-        messages: [{
-            type: "text",
-            text: message
-        }]
-    };
-    return template;
+  let template = {
+    to: to,
+    messages: [
+      {
+        type: "text",
+        text: message
+      }
+    ]
+  };
+  return template;
 }
 
-app.post("/", async(req, res) => {
-            const response = await fetch("https://api.line.me/v2/bot/message/push", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            Authorization: "Bearer kS33WAWpGOQmSABYZ6/jGpT8YP4YvEaALbGm3ldNlqdibzji0cR1xCm1hJ1Ofcd8ICy3rvqayw4qNWtsiDHBZYxzWzL35+27G5coSZxOxT8S3ZbyU2vJqnV9jZHNY+//0Z4tluhOfV39LAQ/4oiguQdB04t89/1O/w1cDnyilFU=" //+ process.env.CHANNEL_ACCESS_TOKEN
-                        },
-                        body: JSON.stringify(
-                                generateMessageStructure(
-                                    req.body.userId,
-                                    `${req.body.message} ${
-          req.body.isCheckIn ? `ในช่วงเวลา ${moment().format("HH:mm")}` : ""
+app.post("/", async (req, res) => {
+  const response = await fetch("https://api.line.me/v2/bot/message/push", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer kS33WAWpGOQmSABYZ6/jGpT8YP4YvEaALbGm3ldNlqdibzji0cR1xCm1hJ1Ofcd8ICy3rvqayw4qNWtsiDHBZYxzWzL35+27G5coSZxOxT8S3ZbyU2vJqnV9jZHNY+//0Z4tluhOfV39LAQ/4oiguQdB04t89/1O/w1cDnyilFU=" //+ process.env.CHANNEL_ACCESS_TOKEN
+    },
+    body: JSON.stringify(
+      generateMessageStructure(
+        req.body.userId,
+        `${req.body.message} ${
+          req.body.isCheckIn
+            ? `ในช่วงเวลา ${moment()
+                .tz("Asia/Bangkok")
+                .format("HH:mm")}`
+            : ""
         }`
       )
     )
