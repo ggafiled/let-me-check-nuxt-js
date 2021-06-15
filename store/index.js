68665cf7322a3c1db1e76e5698cfb054a8988097
ggafiled <gafewkik234@gmail.com>
2021-06-15 07:52:40 +0700

    fix axios base url bug on heroku.

--- store/index.js
+++ store/index.js
@@ -1,320 +1,207 @@
 export const state = () => ({
-    dialog: {
-        isShow: false,
-        title: "",
-        message: ""
-    },
-    authenticated: {
-        userId: "",
-        auth: false
-    },
-    register: {
-        firstName: "",
-        lastName: "",
-        gender: 1,
-        email: "",
-        mobileNumber: "",
-        position: ""
-    },
-    thaichana: {
-        myshop: []
-    },
-    profile: {
-        userId: "",
-        displayName: "",
-        pictureUrl: "",
-        statusMessage: "",
-        language: ""
-    }
+  dialog: {
+    isShow: false,
+    title: "",
+    message: ""
+  },
+  authenticated: {
+    userId: "",
+    auth: false
+  },
+  register: {
+    firstName: "",
+    lastName: "",
+    gender: 1,
+    email: "",
+    mobileNumber: "",
+    position: ""
+  },
+  thaichana: {
+    myshop: []
+  },
+  profile: {
+    userId: "",
+    displayName: "",
+    pictureUrl: "",
+    statusMessage: "",
+    language: ""
+  }
 });
 
 export const getters = {
-    getRegister(state) {
-        return state.register;
-    },
-    getDialog(state) {
-        return state.dialog;
-    },
-    getProfile(state) {
-        return state.profile;
-    },
-    getThaichana(state) {
-        return state.thaichana;
-    },
-    getAuthenticated(state) {
-        return state.authenticated;
-    }
+  getRegister(state) {
+    return state.register;
+  },
+  getDialog(state) {
+    return state.dialog;
+  },
+  getProfile(state) {
+    return state.profile;
+  },
+  getThaichana(state) {
+    return state.thaichana;
+  },
+  getAuthenticated(state) {
+    return state.authenticated;
+  }
 };
 
 export const mutations = {
-    SET_REGISTER(state, data) {
-        state.register = {
-            ...state.register,
-            ...data
-        };
-    },
-    SET_DIALOG(state, data) {
-        state.dialog = {
-            ...state.dialog,
-            ...data
-        };
-    },
-    SET_LINE_PROFILE(state, data) {
-        state.profile = {
-            ...state.profile,
-            ...data
-        };
-    },
-    SET_THAICHANA(state, data) {
-        state.thaichana.myshop = [...data];
-    },
-    SET_INIT_STORE(state, data) {
-        state.authenticated = {
-            ...state.authenticated,
-            ...data
-        };
-    }
+  SET_REGISTER(state, data) {
+    state.register = {
+      ...state.register,
+      ...data
+    };
+  },
+  SET_DIALOG(state, data) {
+    state.dialog = {
+      ...state.dialog,
+      ...data
+    };
+  },
+  SET_LINE_PROFILE(state, data) {
+    state.profile = {
+      ...state.profile,
+      ...data
+    };
+  },
+  SET_THAICHANA(state, data) {
+    state.thaichana.myshop = [...data];
+  },
+  SET_INIT_STORE(state, data) {
+    state.authenticated = {
+      ...state.authenticated,
+      ...data
+    };
+  }
 };
 
 export const actions = {
-    setRegister({ commit }, data) {
-        commit("SET_REGISTER", data);
-    },
-    setDialog({ commit }, data) {
-        commit("SET_DIALOG", data);
-    },
-    setLineProfile({ commit }, data) {
-        commit("SET_LINE_PROFILE", data);
-    },
-    async checkIsRegisted({ commit }) {
+  setRegister({ commit }, data) {
+    commit("SET_REGISTER", data);
+  },
+  setDialog({ commit }, data) {
+    commit("SET_DIALOG", data);
+  },
+  setLineProfile({ commit }, data) {
+    commit("SET_LINE_PROFILE", data);
+  },
+  async checkIsRegisted({ commit }) {
+    try {
+      const profileRef = this.$fire.firestore.collection("/users");
+      const snapshot = await profileRef
+        .where("userId", "==", this.state.profile.userId)
+        .get();
+      if (snapshot.empty) {
+        console.log("No matching documents.");
+        return;
+      }
+
+      snapshot.forEach(doc => {
+        // console.log(doc.id, "=>", doc.data());
+        commit("SET_REGISTER", doc.data());
+      });
+      this.$auth.$storage.setLocalStorage(
+        "authenticated",
+        JSON.stringify({
+          userId: this.state.profile.userId,
+          auth: true
+        })
+      );
+
+      commit("SET_INIT_STORE", {
+        userId: this.state.profile.userId,
+        auth: Boolean(true)
+      });
+    } catch (e) {
+      commit("SET_DIALOG", {
+        isShow: true,
+        title: "Authentication error",
+        message: e.message
+      });
+      return;
+    }
+  },
+  async getThaichana({ commit }) {
+    const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
+    const { userId, auth } = await this.$auth.$storage.getLocalStorage(
+      "authenticated"
+    );
+
+    try {
+      const snapshot = await thaichanaUserRef
+        .where("userId", "==", userId)
+        .get();
+      if (snapshot.empty) {
+        console.log("No matching documents.");
+        return [];
+      }
+
+      let myShops = [];
+      snapshot.forEach(childSnapshot => {
+        myShops.push({
+          userId: childSnapshot.data().userId,
+          appId: childSnapshot.data().appId,
+          businessType: childSnapshot.data().businessType,
+          canCheckin: childSnapshot.data().canCheckin,
+          shopId: childSnapshot.data().shopId,
+          status: childSnapshot.data().status,
+          subcategory: childSnapshot.data().subcategory,
+          title: childSnapshot.data().title
+        });
+      });
+      commit("SET_THAICHANA", myShops);
+    } catch (e) {
+      commit("SET_DIALOG", {
+        isShow: true,
+        title: "Thaichana history error",
+        message: e.message
+      });
+      return;
+    }
+  },
+  async setThaichanaShop({ commit }, data) {
+    const { userId, auth } = await this.$auth.$storage.getLocalStorage(
+      "authenticated"
+    );
+    const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
+    try {
+      const snapshotIsExists = await thaichanaUserRef
+        .where("userId", "==", userId)
+        .where("shopId", "==", data.shopId)
+        .get();
+      if (!snapshotIsExists.empty) {
+        commit("SET_DIALOG", {
+          isShow: true,
+          title: "ขออภัย",
+          message: "ขออภัยค่ะ คุณเพิ่มร้านค้านี้ไปแล้ว."
+        });
+        return;
+      }
+
+      let snapshotCount;
+      const snapshotIsOverLimit = await thaichanaUserRef
+        .where("userId", "==", userId)
+        .get();
+
+      snapshotCount = (await snapshotIsOverLimit.size) + 1;
+
+      if (snapshotCount > 3) {
+        commit("SET_DIALOG", {
+          isShow: true,
+          title: "ขออภัย",
+          message: "ขออภัยค่ะ ตอนนี้คุณมีร้านค้าครบขีดจำกัดแล้ว."
+        });
+        return;
+      } else {
+        await thaichanaUserRef.doc().set(data);
+        commit("SET_DIALOG", {
+          isShow: true,
+          title: "Success",
+          message: `ระบบได้ทำการบันทึกร้านค้า ${data.title} ให้แล้วค่ะ`
+        });
         try {
-            const profileRef = this.$fire.firestore.collection("/users");
-            const snapshot = await profileRef
-                .where("userId", "==", this.state.profile.userId)
-                .get();
-            if (snapshot.empty) {
-                console.log("No matching documents.");
-                return;
-            }
-
-            snapshot.forEach(doc => {
-                // console.log(doc.id, "=>", doc.data());
-                commit("SET_REGISTER", doc.data());
-            });
-            this.$auth.$storage.setLocalStorage(
-                "authenticated",
-                JSON.stringify({
-                    userId: this.state.profile.userId,
-                    auth: true
-                })
-            );
-
-            commit("SET_INIT_STORE", {
-                userId: this.state.profile.userId,
-                auth: Boolean(true)
-            });
-        } catch (e) {
-            commit("SET_DIALOG", {
-                isShow: true,
-                title: "Authentication error",
-                message: e.message
-            });
-            return;
-        }
-    },
-    async getThaichana({ commit }) {
-        const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
-        const { userId, auth } = await this.$auth.$storage.getLocalStorage(
-            "authenticated"
-        );
-
-        try {
-            const snapshot = await thaichanaUserRef
-                .where("userId", "==", userId)
-                .get();
-            if (snapshot.empty) {
-                console.log("No matching documents.");
-                return [];
-            }
-
-            let myShops = [];
-            snapshot.forEach(childSnapshot => {
-                myShops.push({
-                    userId: childSnapshot.data().userId,
-                    appId: childSnapshot.data().appId,
-                    businessType: childSnapshot.data().businessType,
-                    canCheckin: childSnapshot.data().canCheckin,
-                    shopId: childSnapshot.data().shopId,
-                    status: childSnapshot.data().status,
-                    subcategory: childSnapshot.data().subcategory,
-                    title: childSnapshot.data().title
-                });
-            });
-            commit("SET_THAICHANA", myShops);
-        } catch (e) {
-            commit("SET_DIALOG", {
-                isShow: true,
-                title: "Thaichana history error",
-                message: e.message
-            });
-            return;
-        }
-    },
-    async setThaichanaShop({ commit }, data) {
-        const { userId, auth } = await this.$auth.$storage.getLocalStorage(
-            "authenticated"
-        );
-        const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
-        try {
-            const snapshotIsExists = await thaichanaUserRef
-                .where("userId", "==", userId)
-                .where("shopId", "==", data.shopId)
-                .get();
-            if (!snapshotIsExists.empty) {
-                commit("SET_DIALOG", {
-                    isShow: true,
-                    title: "ขออภัย",
-                    message: "ขออภัยค่ะ คุณเพิ่มร้านค้านี้ไปแล้ว."
-                });
-                return;
-            }
-
-            let snapshotCount;
-            const snapshotIsOverLimit = await thaichanaUserRef
-                .where("userId", "==", userId)
-                .get();
-
-            snapshotCount = (await snapshotIsOverLimit.size) + 1;
-
-            if (snapshotCount > 3) {
-                commit("SET_DIALOG", {
-                    isShow: true,
-                    title: "ขออภัย",
-                    message: "ขออภัยค่ะ ตอนนี้คุณมีร้านค้าครบขีดจำกัดแล้ว."
-                });
-                return;
-            } else {
-                await thaichanaUserRef.doc().set(data);
-                commit("SET_DIALOG", {
-                    isShow: true,
-                    title: "Success",
-                    message: `ระบบได้ทำการบันทึกร้านค้า ${data.title} ให้แล้วค่ะ`
-                });
-                try {
-                    var infomation = {
-                        appId: data.appId,
-                        businessType: data.businessType,
-                        canCheckin: data.canCheck,
-                        shopId: data.shopId,
-                        status: data.status,
-                        subcategory: data.subcategory,
-                        title: data.title,
-                        userId: data.userId,
-                        message: `ระบบได้ทำการบันทึกร้านค้า ${data.title} ให้แล้วค่ะ`,
-                        isCheckIn: false
-                    };
-                    const response = this.$axios.$post("/push-message", infomation, {
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    });
-                } catch (e) {
-                    commit("SET_DIALOG", {
-                        isShow: true,
-                        title: "เกิดข้อผิดพลาด",
-                        message: "ไม่สามารถส่งข้อความแจ้งเตือนได้ เนื่องจากพบปัญหาบางอย่าง"
-                    });
-                    return;
-                }
-            }
-        } catch (e) {
-            commit("SET_DIALOG", {
-                isShow: true,
-                title: "Thaichana history error",
-                message: e.message
-            });
-            return;
-        }
-    },
-    async getThaichanaShopNameByToken({ commit }, data) {
-        const response = await this.$axios.$get(
-            `https://api-customer.thaichana.com/shop/${data.appId}/${data.shopId}/qr`
-        );
-        return response;
-    },
-    initialiseStore({ commit }) {
-        if (this.$auth.$storage.getLocalStorage("authenticated")) {
-            const { userId, auth } = JSON.parse(
-                JSON.stringify(this.$auth.$storage.getLocalStorage("authenticated"))
-            );
-            commit("SET_INIT_STORE", {
-                userId: userId,
-                auth: Boolean(auth)
-            });
-        }
-    },
-    async saveRegister({ commit }, data) {
-        const profileRef = this.$fire.firestore.collection("/users");
-
-        try {
-            await profileRef.doc().set(data);
-            var infomation = {
-                appId: data.appId,
-                businessType: data.businessType,
-                canCheckin: data.canCheck,
-                shopId: data.shopId,
-                status: data.status,
-                subcategory: data.subcategory,
-                title: data.title,
-                userId: data.userId,
-                message: `ลงทะเบียนเรียบร้อย ท่านสามารถเพิ่มร้านค้าได้แล้วตอนนี้`,
-                isCheckIn: false
-            };
-            const response = this.$axios.$post("/push-message", infomation, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-
-            console.log("Registed");
-        } catch (e) {
-            commit("SET_DIALOG", {
-                isShow: true,
-                title: "Thaichana history error",
-                message: e.message
-            });
-            return;
-        }
-    },
-    async removeShop({ commit }, data) {
-        const { userId, auth } = await this.$auth.$storage.getLocalStorage(
-            "authenticated"
-        );
-        console.log(`ACTION DELETE ${userId} : ${data.shopId}`);
-        const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
-        try {
-            const snapshotIsExists = await thaichanaUserRef
-                .where("userId", "==", userId)
-                .where("shopId", "==", data.shopId)
-                .get();
-
-            if (!snapshotIsExists.empty) {
-                //TO DO REMOVE
-                snapshotIsExists.forEach(async doc => {
-                    await doc.ref.delete();
-                });
-            }
-        } catch (e) {
-            commit("SET_DIALOG", {
-                isShow: true,
-                title: "Error",
-                message: e.message
-            });
-            return;
-        }
-    },
-    pushMessageToLine({ commit }, data) {
-        var infomation = {
+          var infomation = {
             appId: data.appId,
             businessType: data.businessType,
             canCheckin: data.canCheck,
@@ -323,14 +210,141 @@ export const actions = {
             subcategory: data.subcategory,
             title: data.title,
             userId: data.userId,
-            message: `ระบบได้ทำการเช็คอินร้านค้า ${data.title} ให้แล้วค่ะ`,
-            isCheckIn: true
-        };
-        const response = this.$axios.$post("/push-message", infomation, {
+            message: `ระบบได้ทำการบันทึกร้านค้า ${data.title} ให้แล้วค่ะ`,
+            isCheckIn: false
+          };
+          const response = this.$axios.$post("/push-message", infomation, {
             headers: {
-                "Content-Type": "application/json"
+              "Content-Type": "application/json"
             }
-        });
-        return response;
+          });
+        } catch (e) {
+          commit("SET_DIALOG", {
+            isShow: true,
+            title: "เกิดข้อผิดพลาด",
+            message: "ไม่สามารถส่งข้อความแจ้งเตือนได้ เนื่องจากพบปัญหาบางอย่าง"
+          });
+          return;
+        }
+      }
+    } catch (e) {
+      commit("SET_DIALOG", {
+        isShow: true,
+        title: "Thaichana history error",
+        message: e.message
+      });
+      return;
     }
+  },
+  async getThaichanaShopNameByToken({ commit }, data) {
+    const response = await this.$axios.$get(
+      `https://api-customer.thaichana.com/shop/${data.appId}/${data.shopId}/qr`
+    );
+    return response;
+  },
+  initialiseStore({ commit }) {
+    if (this.$auth.$storage.getLocalStorage("authenticated")) {
+      const { userId, auth } = JSON.parse(
+        JSON.stringify(this.$auth.$storage.getLocalStorage("authenticated"))
+      );
+      commit("SET_INIT_STORE", {
+        userId: userId,
+        auth: Boolean(auth)
+      });
+    }
+  },
+  async saveRegister({ commit }, data) {
+    const profileRef = this.$fire.firestore.collection("/users");
+
+    try {
+      await profileRef.doc().set(data);
+      var infomation = {
+        appId: data.appId,
+        businessType: data.businessType,
+        canCheckin: data.canCheck,
+        shopId: data.shopId,
+        status: data.status,
+        subcategory: data.subcategory,
+        title: data.title,
+        userId: data.userId,
+        message: `ลงทะเบียนเรียบร้อย ท่านสามารถเพิ่มร้านค้าได้แล้วตอนนี้`,
+        isCheckIn: false
+      };
+      const response = this.$axios.$post("/push-message", infomation, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      console.log("Registed");
+    } catch (e) {
+      commit("SET_DIALOG", {
+        isShow: true,
+        title: "Thaichana history error",
+        message: e.message
+      });
+      return;
+    }
+  },
+  async removeShop({ commit }, data) {
+    const { userId, auth } = await this.$auth.$storage.getLocalStorage(
+      "authenticated"
+    );
+    console.log(`ACTION DELETE ${userId} : ${data.shopId}`);
+    const thaichanaUserRef = this.$fire.firestore.collection("thaichana");
+    try {
+      const snapshotIsExists = await thaichanaUserRef
+        .where("userId", "==", userId)
+        .where("shopId", "==", data.shopId)
+        .get();
+
+      if (!snapshotIsExists.empty) {
+        //TO DO REMOVE
+        snapshotIsExists.forEach(async doc => {
+          await doc.ref.delete();
+        });
+      }
+    } catch (e) {
+      commit("SET_DIALOG", {
+        isShow: true,
+        title: "Error",
+        message: e.message
+      });
+      return;
+    }
+  },
+  pushMessageToLine({ commit }, data) {
+    var infomation = {
+      appId: data.appId,
+      businessType: data.businessType,
+      canCheckin: data.canCheck,
+      shopId: data.shopId,
+      status: data.status,
+      subcategory: data.subcategory,
+      title: data.title,
+      userId: data.userId,
+      message: `ระบบได้ทำการเช็คอินร้านค้า ${data.title} ให้แล้วค่ะ`,
+      isCheckIn: true
+    };
+
+    let response;
+    if (process.env.NODE_ENV === "production") {
+      response = this.$axios.$post(
+        process.env.BASE_URL + "/push-message",
+        infomation,
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+    } else {
+      response = this.$axios.$post("/push-message", infomation, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    }
+    return response;
+  }
 };
