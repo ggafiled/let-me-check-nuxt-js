14fba9ffba05d017f3135797959208007a0297a7
ggafiled <gafewkik234@gmail.com>
2021-06-21 21:24:03 +0700

    fix bug

--- server-middleware/cronjob-thaichana.js
+++ server-middleware/cronjob-thaichana.js
@@ -31,10 +31,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,GET,POST, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
